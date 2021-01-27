46c6ad7ff1b51f3037f9a0e22a5ef6a66637cad5
YuriSuheishi <yuri.suheishi@hotmail.com>
2021-01-27 18:04:39 -0300

    conteudo aula 6

--- Lista/get_ex.php
+++ Lista/get_ex.php
@@ -1,8 +1,15 @@
 <?php
 
 if($_GET){
-    $nome = $_GET['nome'];
-    echo 'Nome da pessoa é ' . $nome;
+    // isset() - função interna que verifica se a variável existe
+    // empty() - função interna que verifica/checa se o valor é nulo
+    if(isset($_GET['nome']) && !empty($_GET['nome'])){
+        $nome = $_GET['nome'];
+        echo 'Nome da pessoa é ' . $nome;
+    }
+    else{
+        echo 'Nome não informado!';
+    }
 }
 
 ?>
