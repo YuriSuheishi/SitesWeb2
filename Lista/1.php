ff4b6571d607c2a4592610a64ee10b59bff91235
YuriSuheishi <yuri.suheishi@hotmail.com>
2021-01-28 17:41:51 -0300

    ex 1

---
+++ Lista/1.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+
+    <form action="" class="container" method="POST">
+        <div class="form-group">
+            <label for="x">Variavel</label>
+            <input type="text" name="x" id="x" class="form-control" value=""/>
+        </div>
+        <div class="form-group">
+            <button class="btn btn-primary">
+                Enviar
+            </button>
+        </div>
+    </form>
+
+    <?php
+        $x = $_POST['x'];
+
+    ?>
+    <h1>
+        <?php
+        if($_POST){
+            if(isset($_POST['x']) && !empty($_POST['x'])){
+                if($x > 0){
+                    echo 'O valor é positivo';
+                }
+                elseif($x = 0){
+                    echo 'O valor é igual a zero'
+                }
+                else{
+                    echo 'O valor é negativo'
+                }
+            }
+            else{
+                echo 'Nenhum Valor Atribuido'
+            }
+        }
+        ?>
+    </h1>
+</body>
+</html>
